7bd1f9ebc27fca39b7da5472b4471513db0b5c15
Felie Bento <felipe.16costa@gmail.com>
2020-10-31 22:41:12 -0300

    Correção

--- src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
@@ -1,12 +1,7 @@
 package br.com.alura.listavip.controllers;
 
 import br.com.alura.listavip.models.Convidado;
-import br.com.alura.listavip.repository.ConvidadoRepository;
 import br.com.alura.listavip.services.ConvidadoService;
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,16 +30,17 @@ public class ConvidadoController {
     }
 
     @RequestMapping(value= "salvar", method = RequestMethod.POST)
-    public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email,
-                         @RequestParam("telefone") String telefone, Model model ){
-
+    public String salvar(@RequestParam("nome") String nome,
+                         @RequestParam("email") String email,
+                         @RequestParam("telefone") String telefone,
+                         Model model)
+    {
         Convidado novoConvidado = new Convidado(nome, email, telefone);
         convidadoService.salvar(novoConvidado);
         convidadoService.enviarEmail(nome, email);
 
         Iterable<Convidado> convidados = convidadoService.obterTodos();
         model.addAttribute("convidados", convidados);
-
         return "listaconvidados";
     }
 }
